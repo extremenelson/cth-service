b660e78bbe85c9fddb912914b6e665b44c7c0154
Alex Nelson <alex.nelson@capitalone.com>
2015-08-13 16:16:13 +0000

    Added email send to project.

--- src/cth_service/handler.clj
+++ src/cth_service/handler.clj
@@ -1,10 +1,37 @@
 (ns cth-service.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [cheshire.core :as json]
+            [postal.core :as postal]))
+
+(def user "awsuser")
+(def pass "awspass")
+(def from "alex@extreme-nelsons.net")
+(def to "sklevine407@gmail.com")
+(def host "email-smtp.us-east-1.amazonaws.com")
+(def port 587)
+
+(defn send-email []
+  "Sends an email via AWS SES"
+  (postal/send-message {:user user :pass pass
+                 :host host
+                 :port port}
+                {:from from :to to
+                 :subject "Test from Amazon SES" :body "Test!!!11"}))
+
+(defn post-transaction []
+  "Posts a transaction to SES"
+  (send-email)
+  {:status 200
+   :headers {"Content-Type" "application/json charset=utf-8"}
+   :body (json/generate-string
+           {:disposition "accepted"
+            :status "pending"})})
 
 (defroutes app-routes
   (GET "/" [] "Hello World")
+           (GET "/transactions" [] (post-transaction))
   (route/not-found "Not Found"))
 
 (def app
